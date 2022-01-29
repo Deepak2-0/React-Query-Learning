204776a8da9ac14a911d9bda7d867904eb28e137
Deepak Choudhary <deepak.choudhary@niyotail.com>
2022-01-29 14:23:12 +0530

    axios interceptor done

--- react-query-starter/src/hooks/useMutationSuperHero.js
+++ react-query-starter/src/hooks/useMutationSuperHero.js
@@ -1,12 +1,15 @@
 import { useQuery, useMutation, useQueryClient } from "react-query";
-import axios from "axios";
+// import axios from "axios";
+import { request } from "../axios-utils";
 
 const fetchSuperHeroes = () => {
-    return axios.get("http://localhost:4000/superheroes");
+    // return axios.get("http://localhost:4000/superheroes");
+    return request({ url: "/superheroes" });
 };
 
 const addSuperHero = (hero) => {
-    return axios.post("http://localhost:4000/superheroes", hero);
+    // return axios.post("http://localhost:4000/superheroes", hero);
+    return request({ url: "/superheroes", method: "post", data: hero });
 };
 
 export const useSuperHeroesData = () => {
